777a0e2d0aca88355e26b8b8472641c66f4a1ffe
airmanon <anjali.ghosh@revature.net>
2021-12-14 14:37:01 -0800

    Create package-info.java

---
+++ searchalgorithmdemo/src/main/java/com/revature/searchalgorithmdemo/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author Dipanjali Ghosh
+ *
+ */
+package com.revature.searchalgorithmdemo.model;
+
+/**
+ * This package holds the model class(es) that is going to be used in the demo.
+ */
