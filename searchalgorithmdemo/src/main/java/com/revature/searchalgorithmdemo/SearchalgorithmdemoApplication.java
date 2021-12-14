6f89335a9825140a5ae9e9cc2dca2773b3f9bfcf
airmanon <anjali.ghosh@revature.net>
2021-12-14 14:21:21 -0800

    Create Search Algorithm Demo
    Created the project for demonstrating the code.

---
+++ searchalgorithmdemo/src/main/java/com/revature/searchalgorithmdemo/SearchalgorithmdemoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.searchalgorithmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchalgorithmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchalgorithmdemoApplication.class, args);
+	}
+
+}
