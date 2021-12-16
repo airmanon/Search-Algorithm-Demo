cc8a632578c43c521cfa739c9e75293e0739a290
airmanon <anjali.ghosh@revature.net>
2021-12-16 09:55:26 -0800

    Update SearchalgorithmdemoApplication.java
    Implemented Linear and Binary Searching.

--- searchalgorithmdemo/src/main/java/com/revature/searchalgorithmdemo/SearchalgorithmdemoApplication.java
+++ searchalgorithmdemo/src/main/java/com/revature/searchalgorithmdemo/SearchalgorithmdemoApplication.java
@@ -7,6 +7,7 @@ package com.revature.searchalgorithmdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.revature.searchalgorithmdemo.model.AVLTree;
 import com.revature.searchalgorithmdemo.model.KeyValuePair;
 
 import org.json.simple.JSONArray;
@@ -15,15 +16,21 @@ import org.json.simple.parser.JSONParser;
  
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collections;
+//import java.util.Iterator;
 
 @SpringBootApplication
 public class SearchalgorithmdemoApplication {
 
+	final static String FOUND_MESSAGE_PART_1 = "At index ";
+	final static String FOUND_MESSAGE_PART_2 = " -> ";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SearchalgorithmdemoApplication.class, args);
 		int min = 1;
 		int max = 26;
+		int runs = 5;
+		int myKey = 0; // will be used to hold the number being searched for
 		
 		JSONParser parser = new JSONParser();
 		try {
@@ -39,23 +46,133 @@ public class SearchalgorithmdemoApplication {
 			
 			for(int i = 0; i < keyValueList.size(); i++)
 			{
-				keyValueList.get(i);
-				//
+				JSONObject temp = (JSONObject) keyValueList.get(i);
+				myList.add(new KeyValuePair(new Integer(((Long)temp.get("key")).intValue()), (String)temp.get("value")));
+			}
+			//System.out.println("Before Shuffle: " + myList);
+			ArrayList<KeyValuePair> mySortedList = new ArrayList<KeyValuePair>(myList);
+			Collections.shuffle(myList);
+			//System.out.println("After Shuffle: " + myList);
+			AVLTree myTree = new AVLTree();
+			for(int y = 0; y < myList.size(); y++)
+			{
+				myTree.insert(myList.get(y));
 			}
 			
-			System.out.println(keyValueList);
-			// An iterator over a collection. Iterator takes the place of Enumeration in the Java Collections Framework.
-			// Iterators differ from enumerations in two ways:
-			// 1. Iterators allow the caller to remove elements from the underlying collection during the iteration with well-defined semantics.
-			// 2. Method names have been improved.
-//			Iterator<JSONArray> iterator = keyValueList.iterator();
-//			while (iterator.hasNext()) {
-//				System.out.println(iterator.next());
-//				iterator.
-//			}
+			for(int x = 0; x < runs; x++)
+			{
+				myKey = (int) Math.floor(Math.random()*(max-min+1)+min);
+				System.out.println("\nSearching for KeyValuePair with key " + myKey);
+				try {
+					// linear search for myKey in mySortedList
+					System.out.print("Linear Search in a Sorted List: ");
+					System.out.println(searchLinear(mySortedList, myKey));
+				}
+				catch (Exception e)
+				{
+					System.err.println(e.getMessage());
+				}
+				
+				try {
+					// linear search for myKey in myList
+					System.out.print("Linear Search in an Unsorted List: ");
+					System.out.println(searchLinear(myList, myKey));
+				}
+				catch (Exception e)
+				{
+					System.err.println(e.getMessage());
+				}
+				
+				try {
+					// binary search for myKey in mySortedList
+					System.out.print("Binary Search in a Sorted List: ");
+					System.out.println(searchBinary(mySortedList, myKey));
+				}
+				catch (Exception e)
+				{
+					System.out.println(e.getMessage());
+					//System.err.println(e.getMessage());
+				}
+				
+				try {
+					// binary search for myKey in myList
+					System.out.print("Binary Search in an Unsorted List: ");
+					System.out.println(searchBinary(myList, myKey));
+				}
+				catch (Exception e)
+				{
+					System.out.println(e.getMessage());
+					//System.err.println(e.getMessage());
+				}
+				System.out.println();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static KeyValuePair searchLinear(ArrayList<KeyValuePair> aList, int key) throws Exception
+	{
+		for(int x = 0; x < aList.size(); x++)
+		{
+			if(aList.get(x).getKey() == key)
+			{
+				System.out.print(FOUND_MESSAGE_PART_1 + x + FOUND_MESSAGE_PART_2);
+				return aList.get(x);
+			}
+		}
+		throw new Exception("Key value " + key + " not found!");
+	}
+	
+	public static KeyValuePair searchBinary(ArrayList<KeyValuePair> aList, int key) throws Exception
+	{
+		if(aList.isEmpty())
+		{
+			throw new Exception("No keys in the list!");
+		}
+		return searchBinary(aList, key, 0, aList.size());
+	}
+	
+	public static KeyValuePair searchBinary(ArrayList<KeyValuePair> aList, int key, int minIndex, int maxIndex) throws Exception
+	{
+		//System.out.println("minIndex = " + minIndex + ", maxIndex = " + maxIndex);
+		int temp = (maxIndex + minIndex) / 2;
+		if(temp >= maxIndex) // meaning that we're out of bounds
+		{
+			throw new Exception("Key value " + key + " not found! Halfway point out of bounds!");
+		}
+		
+		if(temp == minIndex)
+		{
+			if(aList.get(minIndex).getKey() == key)
+			{
+				System.out.print(FOUND_MESSAGE_PART_1 + minIndex + FOUND_MESSAGE_PART_2);
+				return aList.get(minIndex);
+			}
+			else
+			{
+				throw new Exception("Key value " + key + " not found!");
+			}
+		}
+		
+		KeyValuePair tempPair = aList.get(temp);
+		int tempKey = tempPair.getKey();
+		//System.out.println("temp = " + temp + ", tempPair = " + tempPair + ", tempKey = " + tempKey);
+		if(key == tempKey)
+		{
+			System.out.print(FOUND_MESSAGE_PART_1 + temp + FOUND_MESSAGE_PART_2);
+			return tempPair;
+		}
+		else
+		{
+			if(key < tempKey) // meaning that we're looking for something smaller than what we found.
+			{
+				return searchBinary(aList, key, minIndex, temp);
+			}
+			else
+			{
+				return searchBinary(aList, key, temp, maxIndex);
+			}
+		}
+	}
 }
